dfce39a540fce7c946865b892f8418357259cefa
reuben <reuben@ubuntu22-04.myguest.virtualbox.org>
2025-05-01 15:21:04 +0530

    initialize order book

--- clob/programs/clob/src/instructions/initialize_orderbook.rs
+++ clob/programs/clob/src/instructions/initialize_orderbook.rs
@@ -1,4 +1,10 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
+
+use crate::state::Orderbook;
 
 #[derive(Accounts)]
 pub struct InitializeOrderbook<'info> {
@@ -7,10 +13,43 @@ pub struct InitializeOrderbook<'info> {
     pub base_token_mint: InterfaceAccount<'info, Mint>,
     pub quote_token_mint: InterfaceAccount<'info, Mint>,
 
+    #[account(init,
+        payer = payer,
+        associated_token::mint = base_token_mint,
+        associated_token::authority = order_book,
+        associated_token::token_program = token_program,
+        )]
+    pub base_vault: InterfaceAccount<'info, TokenAccount>,
 
+    #[account(init,
+        payer = payer,
+        associated_token::mint = quote_token_mint,
+        associated_token::authority = order_book,
+        associated_token::token_program = token_program,
+        )]
+    pub quote_vault: InterfaceAccount<'info, TokenAccount>,
+    
+    #[account(init,
+        payer = payer,
+        space = 8 + Orderbook::INIT_SPACE,
+        seeds = [b"orderbook",base_token_mint.key().as_ref(),quote_token_mint.key().as_ref()],
+        bump)]
+    pub order_book: Account<'info, Orderbook>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info, TokenInterface>,
     pub system_program: Program<'info, System>,
 }
 
-pub fn handler(ctx: Context<InitializeOrderbook>) -> Result<()> {
+pub fn handler(context: Context<InitializeOrderbook>) -> Result<()> {
+    context.accounts.order_book.set_inner(Orderbook { base_asset: context.accounts.base_token_mint.key(), 
+        quote_asset: context.accounts.quote_token_mint.key(), 
+        base_vault: context.accounts.base_vault.key(), 
+        quote_vault: context.accounts.quote_vault.key(), 
+        base_decimals: context.accounts.base_token_mint.decimals, 
+        quote_decimals: context.accounts.quote_token_mint.decimals, 
+        authority: context.accounts.payer.key(), 
+        bump: context.bumps.order_book 
+    });
     Ok(())
 }
